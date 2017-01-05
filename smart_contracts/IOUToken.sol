1e08ee634f21e5ae855cc00f00276f00cca1eea4
Heiko Heiko <heiko@heiko.org>
2017-01-05 15:50:25 +0100

    add balanceOf

--- smart_contracts/IOUToken.sol
+++ smart_contracts/IOUToken.sol
@@ -8,18 +8,14 @@ contract IOUToken {
 
     event Transferred(address sender, address receiver, int amount);
     event Approved(address account);
-    event Disapproved(address account);
-
-    modifier onlyOwner() {
-        if (msg.sender != owner) {
-            throw;
-        } else {
-            _;
-        }
-    }
 
     function IOUToken() {
         owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        if (msg.sender != owner) throw;
+        _;
     }
 
     // transfer from user to user
@@ -40,7 +36,12 @@ contract IOUToken {
         _transfer(amount, sender, receiver);
     }
 
-    function updateBalance(address account, int amount) private {
+    function balanceOf(address account) constant returns uint {
+        return balances[account];
+    }
+
+    // helper to check for overflows
+    function _updateBalance(address account, int amount) private {
         int old_balance = balances[account];
         int new_balance = old_balance + amount;
         // Check for positive value overflow
@@ -55,15 +56,28 @@ contract IOUToken {
         balances[account] += amount;
     }
 
+    function _transfer (int amount, address sender address receiver) {
+        if (amount < 0) throw;
+        _updateBalance(sender, -amount);
+        _updateBalance(receiver, amount);
+        Transferred(sender, receiver, amount);
+    }
+
+    // transfer from user to user
+    function transfer (int amount, address receiver) {
+        _transfer(amount, msg.sender, receiver);
+    }
+
+    // delegated transfers by approved account
+    function transfer_by_delegate(int amount, address sender, address receiver) {
+        if (!approved_accounts[msg.sender]) throw; // check delegate is approved
+        _transfer(amount, sender, receiver);
+    }
+
     // IOU Token owner can whitelist an account
     function approve_account(address account) onlyOwner {
         approved_accounts[account] = true;
         Approved(account);
     }
 
-    // IOU Token owner can unwhitelist an account
-    function disapprove_account(address account) onlyOwner {
-        approved_accounts[account] = false;
-        Disapproved(account);
-    }
 }
