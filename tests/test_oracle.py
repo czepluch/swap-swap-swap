6c06f5c4199ce34c3dd91e483ae19ad4c2c0887f
Jacob Stenum Czepluch <j.czepluch@gmail.com>
2017-01-05 17:28:57 +0100

    updating test for oracle and using auto generated getters

--- tests/test_oracle.py
+++ tests/test_oracle.py
@@ -1,5 +1,6 @@
 # -*- coding: utf8 -*-
 import pytest
+import time
 from os.path import dirname, abspath, join, realpath
 
 from ethereum import tester
@@ -13,16 +14,18 @@ def get_contract_path(contract_name):
 
 
 def test_oracle():
-    oracle_path = get_contract_path('InterestRateOracle.sol')
+    oracle_path = get_contract_path('Oracle.sol')
 
     state = tester.state()
     oracle = state.abi_contract(
         None,
         path=oracle_path,
         language='solidity',
-        # constructor_parameters=[],
+        # constructor_parameters=[tester.a0.encode('hex')],
     )
 
-    assert oracle.getInterestRate("USD") == 135
-    assert oracle.getInterestRate("EUR") == 129
-    assert oracle.getInterestRate("US") == 133
+    assert oracle.owner() == tester.a0.encode('hex')
+    oracle.update(int(time.time()), 123)
+
+    assert oracle.getdate() == int(time.time())
+    assert oracle.getFloatingRate() == 123
