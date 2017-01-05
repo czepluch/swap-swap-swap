857801abc84d4d73bacb0180a52fcdf988d05954
Jacob Stenum Czepluch <j.czepluch@gmail.com>
2017-01-05 12:53:57 +0100

    updated IOUToken

--- smart_contracts/IouToken.sol
+++ smart_contracts/IouToken.sol
@@ -1,63 +1,63 @@
 pragma solidity ^0.4.0;
 
-contract owned {
+contract Owned {
     address owner;
-    function owned() {
+
+    modifier onlyOwner() {
+        if (msg.sender != owner) {
+            throw;
+        } else {
+            _;
+        }
+    }
+
+    function Owned() {
         owner = msg.sender;
     }
 
-    function changeOwner(address newOwner) onlyowner {
+    function changeOwner(address newOwner) onlyOwner {
         owner = newOwner;
-    }
-
-    modifier onlyowner() {
-        if (msg.sender == owner) _ ;
     }
 }
 
-contract Iou_token is owned {
-    mapping (address => bool) public valid_swap_contracts;
+contract IOUToken is Owned {
+    mapping (address => bool) public approved_accounts;
     mapping (address => int) public balances;
 
-    event Directly_transferred(address sender, address receiver, uint amount);
-    event Transferred_by_swap_contract(address sender, address receiver, int amount);
-    event Approved_swap_contract(address smart_contract);
-    event Blocked_swap_contract(address smart_contract);
-
-    string public name;
-
-    // Initializing the Token
-    function Iou_token (string _name) {
-        name = _name;
-    }
+    event TransferredDirectly(address sender, address receiver, int amount);
+    event TransferredByApproved(address sender, address receiver, int amount);
+    event Approved(address account);
+    event Disapproved(address account);
 
     // Direct transfer from user to user
-    function transfer (uint amount, address receiver) {
-        //TODO check for overflow
-        balances[msg.sender] -= int(amount);
-        balances[receiver] += int(amount);
-        Directly_transferred(msg.sender, receiver, amount);
+    function transfer (int amount, address receiver) {
+        if (amount < 0) throw;
+        updateBalance(msg.sender, -amount);
+        updateBalance(receiver, amount);
+        TransferredDirectly(msg.sender, receiver, amount);
     }
 
-    // Transer initiated by a SC
-    function transfer_from_sc (int amount, address sender, address receiver) {
-        if(valid_swap_contracts[msg.sender] == true) {
-            balances[msg.sender] -= amount;
-            balances[receiver] += amount;
-            Transferred_by_swap_contract(sender, receiver, amount);
+    function updateBalance(address account, int amount) private {
+        int old_balance = balances[account];
+        int new_balance = old_balance + amount;
+        if (amount > 0 && new_balance < old_balance) {
+            throw;
         }
-        else throw;
+        if (amount < 0 && new_balance > old_balance) {
+            throw;
+        }
+        balances[account] += amount;
     }
 
     // IOU Token owner can whitelist sc
-    function approve_swap_contract (address swap_contract_address) onlyowner {
-        valid_swap_contracts[swap_contract_address] = true;
-        Approved_swap_contract(swap_contract_address);
+    function approve_swap_contract (address swap_contract_address) onlyOwner {
+        approved_accounts[swap_contract_address] = true;
+        Approved(swap_contract_address);
     }
 
     // IOU Token owner can blacklist sc
-    function block_swap_contract (address swap_contract_address) onlyowner {
-        valid_swap_contracts[swap_contract_address] = false;
-        Blocked_swap_contract(swap_contract_address);
+    function block_swap_contract (address swap_contract_address) onlyOwner {
+        approved_accounts[swap_contract_address] = false;
+        Disapproved(swap_contract_address);
     }
 }
